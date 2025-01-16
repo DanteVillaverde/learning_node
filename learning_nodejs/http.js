0c265760f95cb413b4c47c3e4923f4b82bddd7d2
dante.villaverde <dante.jesus.villaverde.rojas@gmail.com>
2025-01-15 22:11:26 -0500

    methods  GET POST And EXPRESS

--- learning_nodejs/http.js
+++ learning_nodejs/http.js
@@ -1,44 +1,58 @@
 const http = require("node:http")
 const fs = require("node:fs")
-const picocolors = require("picocolors");
 const desiredPort = process.argv[2] ?? 0;
 
 const server = http.createServer((request, response) => {
+    switch (request.method) {
+        case 'GET':
+            if (request.url == '/') {
+                response.statusCode = 200;
+                response.setHeader('Content-type', 'text/html')
+                response.end(`
+                    <h1> PAGINA WEB FACTURAS ELECTRONICAS</h1>
+                `)
+            }else if (request.url == '/invoice') {
+                response.statusCode = 200;
+                
+                fs.readFile("./html/invoice.html","utf-8",(error, html) => {
+                    if (error) {
+                        console.log(picocolors.red("ERROR EN EL DIRECTORIO !!"))
+                        return;
+                    }else {
+                        response.setHeader('Content-type', 'text/html')
+                        response.end(html)
+                    }
+                })
+            }else if (request.url == '/show_invoice') {
+                fs.readFile("./html/show_invoice.html", "utf-8",(error,html) =>{
+                    if (error) {
+                        console.log(picocolors.red("ERROR EN EL DIRECTORIO !!"))
+                        return;
+                    }else {
+                        response.setHeader('Content-type', 'text/html')
+                        response.end(html)
+                    }
+                })
+            }else{
+                response.statusCode = 404;
+                response.end(`
+                    404 : UPS PAGE NOT FOUND
+                `)
+            }
+
+            break;
+
+        case 'POST':
+            if (request.url == '/post') {
+                
+            }
+            break;
     
-    if (request.url == '/') {
-        response.statusCode = 200;
-        response.setHeader('Content-type', 'text/html')
-        response.end(`
-            <h1> PAGINA WEB FACTURAS ELECTRONICAS</h1>
-        `)
-    }else if (request.url == '/invoice') {
-        response.statusCode = 200;
-        
-        fs.readFile("./html/invoice.html","utf-8",(error, html) => {
-            if (error) {
-                console.log(picocolors.red("ERROR EN EL DIRECTORIO !!"))
-                return;
-            }else {
-                response.setHeader('Content-type', 'text/html')
-                response.end(html)
-            }
-        })
-    }else if (request.url == '/show_invoice') {
-        fs.readFile("./html/show_invoice.html", "utf-8",(error,html) =>{
-            if (error) {
-                console.log(picocolors.red("ERROR EN EL DIRECTORIO !!"))
-                return;
-            }else {
-                response.setHeader('Content-type', 'text/html')
-                response.end(html)
-            }
-        })
-    }else{
-        response.statusCode = 404;
-        response.end(`
-            404 : UPS PAGE NOT FOUND
-        `)
-    }
+        default:
+            break;
+    } 
+    
+    
    
 })
 
